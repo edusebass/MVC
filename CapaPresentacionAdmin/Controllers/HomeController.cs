a6f5beaed450a8ae48efbc7df5a6bb659fda9188
edusebass <edu03sebas@gmail.com>
2024-09-27 17:26:03 -0500

    clases de modelos SQL y adaptacion de vistas

--- CapaPresentacionAdmin/Controllers/HomeController.cs
+++ CapaPresentacionAdmin/Controllers/HomeController.cs
@@ -12,25 +12,10 @@ namespace CapaPresentacionAdmin.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
+        public ActionResult Usuarios()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-        public ActionResult PaginaTest()
-        {
-            ViewBag.Message = "Your contacdsfsdft page.";
-
-            return View();
-        }
     }
 }
